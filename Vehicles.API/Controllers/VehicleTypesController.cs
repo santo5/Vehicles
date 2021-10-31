98d3d0e809bebd176a2a185533cb121b3d3e34f0
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2021-10-30 23:19:15 -0500

    Improve vehicle Type controller

---
+++ Vehicles.API/Controllers/VehicleTypesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public VehicleTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VehicleTypes.ToListAsync());
+        }
+
+        // GET: VehicleTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleType);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(vehicleType);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehículo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(vehicleType);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleType);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleType);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehículo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(vehicleType);
+        }
+
+        // GET: VehicleTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            _context.VehicleTypes.Remove(vehicleType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool VehicleTypeExists(int id)
+        {
+            return _context.VehicleTypes.Any(e => e.Id == id);
+        }
+    }
+}
